767084d7a48d8235976d6ee77ad4ad1435c409f1
KouT127 <kou26haru@gmail.com>
2020-02-09 17:17:11 +0900

    modify: add splash

---
+++ lib/pages/splash/splash_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Splash'),
+      ),
+    );
+  }
+}
