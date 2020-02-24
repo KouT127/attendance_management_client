04b0825d1b66e2f03dedfe111ada5059c3df3012
KouT127 <kou26haru@gmail.com>
2020-02-24 15:05:39 +0900

    modify: update list page

--- lib/pages/attendance_list/attendance_list_page.dart
+++ lib/pages/attendance_list/attendance_list_page.dart
@@ -1,3 +1,4 @@
+import 'package:attendance_management/widgets/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -7,12 +8,67 @@ class AttendanceListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: ListView.builder(
-        itemCount: 2,
-        itemBuilder: (context, index) {
-          return Placeholder();
-        },
+      body: Container(
+        color: SkyBlue,
+        child: SafeArea(
+          child: CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                iconTheme: Theme.of(context).iconTheme,
+                backgroundColor: SkyBlue,
+                expandedHeight: 20.0,
+                floating: false,
+                pinned: false,
+                flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Text(
+                    "History",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  background: Container(
+                    color: SkyBlue,
+                  ),
+                ),
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    return DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                      ),
+                      child: FractionallySizedBox(
+                        widthFactor: .9,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            const SizedBox(height: 20),
+                            Text(
+                              index.toString(),
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .body1
+                                  .copyWith(color: Colors.black),
+                            ),
+                            const SizedBox(height: 20),
+                            Divider(
+                              height: 2,
+                              color: Colors.grey,
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                  childCount: 30,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
