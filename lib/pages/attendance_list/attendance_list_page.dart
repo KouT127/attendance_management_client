ebe4143fc6110d6f37a781ced4503ff23b3bfa8d
Koichi Tada <kou26haru@gmail.com>
2020-03-03 14:10:02 +0900

    modify: use tabbar scaffold

--- lib/pages/attendance_list/attendance_list_page.dart
+++ lib/pages/attendance_list/attendance_list_page.dart
@@ -1,104 +1,85 @@
+import 'package:attendance_management/widgets/TabBarScaffold.dart';
 import 'package:attendance_management/widgets/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
 class AttendanceListPage extends StatelessWidget {
   static String path = '/attendances';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: SkyBlue,
-        child: SafeArea(
-          child: CustomScrollView(
-            slivers: <Widget>[
-              SliverAppBar(
-                iconTheme: Theme.of(context).iconTheme,
-                backgroundColor: SkyBlue,
-                expandedHeight: 20.0,
-                floating: false,
-                pinned: false,
-                flexibleSpace: FlexibleSpaceBar(
-                  centerTitle: true,
-                  title: Text(
-                    "12月",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
+    final notifier = Provider.of<TabBarNotifier>(context);
+
+    return TabBarScaffold(
+      controller: notifier.controller,
+      itemCount: notifier.tabs.length,
+      initialPosition: notifier.position,
+      title: notifier.title,
+      tabViewBuilder: (index) {
+        return DecoratedBox(
+          decoration: BoxDecoration(
+            color: SkyBlue,
+          ),
+          child: SafeArea(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return FractionallySizedBox(
+                  widthFactor: .95,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.black12,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Row(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 10),
+                              child: DecoratedBox(
+                                decoration: BoxDecoration(
+                                  color: Colors.white70,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: buildLeftItem(context),
+                              ),
+                            ),
+                            Expanded(
+                              child: Column(
+                                children: <Widget>[
+                                  Text('IN'),
+                                  Text(
+                                    "10:00",
+                                    style: Theme.of(context).textTheme.body1,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: Column(
+                                children: <Widget>[
+                                  Text('OUT'),
+                                  Text(
+                                    "19:00",
+                                    style: Theme.of(context).textTheme.body1,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
-                  background: Container(
-                    color: SkyBlue,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget buildSliverList(BuildContext context) {
-    return SliverList(
-      delegate: SliverChildBuilderDelegate(
-        (BuildContext context, int index) {
-          return FractionallySizedBox(
-            widthFactor: .95,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  color: Colors.black12,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10),
-                        child: DecoratedBox(
-                          decoration: BoxDecoration(
-                            color: Colors.white70,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: buildLeftItem(context),
-                        ),
-                      ),
-                      Expanded(
-                        child: Column(
-                          children: <Widget>[
-                            Text('IN'),
-                            Text(
-                              "10:00",
-                              style: Theme.of(context).textTheme.body1,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        child: Column(
-                          children: <Widget>[
-                            Text('OUT'),
-                            Text(
-                              "19:00",
-                              style: Theme.of(context).textTheme.body1,
-                            ),
-                          ],
-                        ),
-                      ),
-                      buildSliverList(context),
-                    ],
-                  ),
-                ),
-              ),
+                );
+              },
             ),
-          );
-        },
-        childCount: 30,
-      ),
+          ),
+        );
+      },
     );
   }
 
