6c83afdf05c6002337a4422d4b67ae3a58c12fd1
KouT127 <kou26haru@gmail.com>
2020-03-15 02:04:45 +0900

    modify: add popRoot function

--- lib/services/app_navigator.dart
+++ lib/services/app_navigator.dart
@@ -1,9 +1,8 @@
-import 'dart:async';
-
 import 'package:attendance_management/pages/attendance_list/attendance_list_page.dart';
 import 'package:attendance_management/pages/pages.dart';
 import 'package:attendance_management/pages/user/user_page.dart';
 import 'package:attendance_management/utils/route.dart';
+import 'package:attendance_management/widgets/protected_route.dart';
 import 'package:attendance_management/widgets/tab_bar_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -16,19 +15,18 @@ class AppNavigator {
   final GlobalKey<NavigatorState> navigatorKey;
 
   Route<dynamic> onGenerate(RouteSettings settings) {
-    if (settings.name == '/login') {
+    if (settings.name == '/settings') {
       return FadeRoute(
-        builder: (_) => LoginPageProvider(),
-      );
-    }
-    if (settings.name == '/user') {
-      return FadeRoute(
-        builder: (_) => UserPage(),
+        builder: (_) => ProtectedRoute(
+          child: SettingsProvider(),
+        ),
       );
     }
     if (settings.name == '/home') {
       return FadeRoute(
-        builder: (_) => HomePageProvider(),
+        builder: (_) => ProtectedRoute(
+          child: HomePageProvider(),
+        ),
       );
     }
 
@@ -40,30 +38,36 @@ class AppNavigator {
       );
     }
 
+    if (settings.name == '/login') {
+      return FadeRoute(
+        builder: (_) => LoginPageProvider(),
+      );
+    }
+
     return MaterialPageRoute(
       builder: (_) => SplashPageProvider(),
     );
   }
 
-  Future<void> push<T>(Route<T> route) async {
-    scheduleMicrotask(() => {navigatorKey.currentState.push<T>(route)});
+  void push<T>(Route<T> route) {
+    navigatorKey.currentState.push<T>(route);
   }
 
-  Future<void> pushNamed(name, {Object arguments}) async {
-    scheduleMicrotask(() => {
-          navigatorKey.currentState.pushNamed(
-            name,
-            arguments: arguments,
-          )
-        });
+  void pushNamed(name, {Object arguments}) {
+    navigatorKey.currentState.pushNamed(
+      name,
+      arguments: arguments,
+    );
   }
 
-  Future<void> pushReplacementNamed(name, {Object arguments}) async {
-    scheduleMicrotask(() => {
-          navigatorKey.currentState.pushReplacementNamed(
-            name,
-            arguments: arguments,
-          )
-        });
+  void pushReplacementNamed(name, {Object arguments}) {
+    navigatorKey.currentState.pushReplacementNamed(
+      name,
+      arguments: arguments,
+    );
+  }
+
+  void popRoot() {
+    navigatorKey.currentState.popUntil((route) => route.isFirst);
   }
 }
