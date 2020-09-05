77b6d1f8d2ee7853bbe168d99305677523f8cb0a
KouT127 <kou26haru@gmail.com>
2020-09-05 23:42:24 +0900

    modify: Export pages

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -1,3 +1,7 @@
+export 'attendance_detail/attendance_detail_page.dart';
+export 'attendance_input/attendance_input_page.dart';
 export 'home/home_page.dart';
 export 'login/login_page.dart';
+export 'profile/profile_page.dart';
+export 'setting/setting_page.dart';
 export 'splash/splash_page.dart';
--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,4 +1,6 @@
 export 'app_navigator.dart';
+export 'app_router.dart';
 export 'auth_service.dart';
 export 'http_client_service.dart';
 export 'shared_preference_service.dart';
+export 'theme.dart';
