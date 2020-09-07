14b6f18327423f6e90e516af7775ecf4b9cd2c65
KouT127 <kou26haru@gmail.com>
2020-09-07 22:14:57 +0900

    refactor

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -3,8 +3,8 @@ import 'package:simple_logger/simple_logger.dart';
 
 final logger = SimpleLogger();
 
-final dateFormatter = DateFormat("y/MM/dd");
-final timeFormatter = DateFormat("HH:mm:ss");
+final dateFormatter = DateFormat('y/MM/dd');
+final timeFormatter = DateFormat('HH:mm:ss');
 
 extension DatetimeEx on DateTime {
   String get formattedDate => dateFormatter.format(this);
